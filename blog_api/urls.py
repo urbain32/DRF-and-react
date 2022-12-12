1a6fdc93aeb1d3a500e784078430262917925841
urbain32 <urbainq32@gmail.com>
2022-12-12 17:22:27 +0200

    creating url path

---
+++ blog_api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+# from .views import PostList,PostDetail
+app_name = 'blog_api'
+
+urlpatterns=[
+#     path('<int:pk>/',PostDetail.as_view(template_name = "detailcreate")),
+#     path('',PostList.as_view(template_name = "listlcreate")),
+]
