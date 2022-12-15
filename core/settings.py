5222665a10b93cb2f10f031868abeb9af0df1cf7
urbain32 <urbainq32@gmail.com>
2022-12-15 07:57:35 +0200

    learning api reference permission in rest.

--- core/settings.py
+++ core/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# the reference of API Reference for django rest
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permissions.AllowAny',
+    ]
+}
